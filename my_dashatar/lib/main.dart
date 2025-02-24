f1f62439bbf64a2de7e9c12dfad500125ab87cde
Wiktor <wiko700@gmail.com>
2025-02-24 19:02:16 +0100

    Understanding Flutter layouts in depth

--- my_dashatar/lib/main.dart
+++ my_dashatar/lib/main.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.amber,
-          appBar: AppBar(
-              backgroundColor: Color(0xFF000000),
-              centerTitle: true,
-              title: Text(
-                "My Dashatar App",
-                style: TextStyle(color: Colors.white),
-              )),
-          body: Center(
-            child: Image.asset("images/dash.png"),
-          ),
-        )),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Text> texts = [Text("Dash 1"), Text("Dash 2")];
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color(0xFF000000),
+            centerTitle: true,
+            title: texts[0]),
+        body: Stack(
+          children: [
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.greenAccent
+            ),
+            Positioned(
+              left: 10.0,
+              top: 20.0,
+              child: Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.pinkAccent
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
 }
